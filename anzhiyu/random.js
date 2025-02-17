9b2566cdc31e8f27db5726dfd1c2d5881aa7db85
clear668866x6 <2523739498@qq.com>
2025-02-17 14:30:08 +0800

    Site updated: 2025-02-17 14:30:08

--- anzhiyu/random.js
+++ anzhiyu/random.js
@@ -1,3 +1,3 @@
-var posts=["2025/02/14/整理/","2025/02/05/每日练习/"];function toRandomPost(){
+var posts=["2025/02/14/整理/","2025/02/05/每日练习/","2025/02/17/知识点系列——基础数据结构/","2025/02/15/队内训练1/","2025/02/15/队内训练2/"];function toRandomPost(){
     pjax.loadUrl('/'+posts[Math.floor(Math.random() * posts.length)]);
   };
